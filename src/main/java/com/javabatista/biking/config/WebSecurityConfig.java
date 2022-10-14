2393928e8f66e85d827d0aa50924c05a0e7df7e9
java-batista <java.batista@outlook.com>
2022-10-14 13:41:01 -0300

    TestController adicionado e configurações de segurança ajustadas

--- src/main/java/com/javabatista/biking/config/WebSecurityConfig.java
+++ src/main/java/com/javabatista/biking/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,18 +19,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private SecurityDatabaseService securityService;
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+        auth.userDetailsService(securityService).passwordEncoder(passwordEncoder);
     }
+
+    private static final String[] SWAGGER_WHITELIST = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+    };
+
+    private static final String[] DEBUG_WHITELIST = {
+            "/"
+    };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers(SWAGGER_WHITELIST).permitAll()
+                .antMatchers(DEBUG_WHITELIST).permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers(HttpMethod.POST,"/login").permitAll()
-//                .antMatchers("/admin").hasAnyRole("MANAGERS")
-//                .antMatchers("/users").hasAnyRole("USERS","MANAGERS")
+                .antMatchers("/admin").hasAnyRole("MANAGERS")
+                .antMatchers("/users").hasAnyRole("USERS","MANAGERS")
                 .anyRequest().authenticated().and().httpBasic();
 
         http.csrf().disable();
