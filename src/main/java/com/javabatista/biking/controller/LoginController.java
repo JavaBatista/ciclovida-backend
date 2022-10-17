3ef5f476a9fb4b1062b0756f2c5a9be8470f0638
java-batista <java.batista@outlook.com>
2022-10-17 12:21:53 -0300

    Obtenção do usuário por RequestHeader

--- src/main/java/com/javabatista/biking/controller/LoginController.java
+++ src/main/java/com/javabatista/biking/controller/LoginController.java
@@ -6,9 +6,13 @@ import com.javabatista.biking.model.User;
 import com.javabatista.biking.repository.UserRepository;
 import com.javabatista.biking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.Base64;
 
 @RestController
 public class LoginController {
@@ -18,16 +22,24 @@ public class LoginController {
     private UserRepository repository;
 
     @GetMapping("/login")
-    public UserRequest logar(@RequestParam(value = "username") String username) {
+    public ResponseEntity<UserRequest> logar(@RequestHeader(HttpHeaders.AUTHORIZATION) String header) {
+        String[] parts = header.split(" ", 2);
+        byte[] decodedBytes = Base64.getDecoder().decode(parts[1]);
+        String authorization = new String(decodedBytes);
+
+        parts = authorization.split(":", 2);
+        String username = parts[0];
+
         UserRequest response = new UserRequest();
         User user = repository.findByUsername(username);
-
         if(user!=null) {
             response.setUserId(user.getId());
             response.setName(user.getName());
             response.setDate(LocalDate.now());
+            return  new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
         }
 
-    return  response;
     }
 }
