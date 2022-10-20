75ab6e907985a13179ff5a83ee1e9fc02cb626b1
java-batista <java.batista@outlook.com>
2022-10-20 17:08:07 -0300

    CrossOrigin para criação de usuários

--- src/main/java/com/javabatista/biking/controller/AdminController.java
+++ src/main/java/com/javabatista/biking/controller/AdminController.java
@@ -3,17 +3,16 @@ package com.javabatista.biking.controller;
 import com.javabatista.biking.model.User;
 import com.javabatista.biking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
     @Autowired
     private UserService service;
+
     @PostMapping("/create_user")
+    @CrossOrigin
     public void postUser(@RequestBody User user){
         service.createUser(user);
     }
